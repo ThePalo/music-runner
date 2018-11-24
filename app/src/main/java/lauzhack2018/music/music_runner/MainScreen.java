298782769c3406caab8dec9cd019f604a8606944
corico44 <cairigaray999@gmail.com>
2018-11-24 18:37:45 +0100

    Button updated

--- app/src/main/java/lauzhack2018/music/music_runner/MainScreen.java
+++ app/src/main/java/lauzhack2018/music/music_runner/MainScreen.java
@@ -1,13 +1,28 @@
 package lauzhack2018.music.music_runner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
-
+    private Button playlist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        playlist=findViewById(R.id.playlist);
+        playlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPlay();
+            }
+        });
+    }
+    public void openPlay(){
+        Intent intent=new Intent(this,Player.class);
+        startActivity(intent);
     }
 }
