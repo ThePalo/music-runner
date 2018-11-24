92f35e9613702ada68a9a59cf562ef9c16693e75
Gorka Piñol <heyslide@users.noreply.github.com>
2018-11-24 12:53:00 +0100

    Initial commit

---
+++ app/src/androidTest/java/lauzhack2018/music/music_runner/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package lauzhack2018.music.music_runner;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("lauzhack2018.music.music_runner", appContext.getPackageName());
+    }
+}
