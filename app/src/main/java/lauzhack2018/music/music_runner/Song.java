40e05c290134f8d9268fc75da3938a3b60a10f65
ThePalo <36154609+ThePalo@users.noreply.github.com>
2018-11-24 21:36:03 +0100

    Knapsack Mk.I

--- app/src/main/java/lauzhack2018/music/music_runner/Song.java
+++ app/src/main/java/lauzhack2018/music/music_runner/Song.java
@@ -1,4 +1,40 @@
 package lauzhack2018.music.music_runner;
 
 public class Song {
+    long id;
+    String title;
+    int time; //in  seconds
+    int value;
+    String artist;
+    //image??
+
+    public Song (long id, String title, int time, int value, String artist) {
+        this.id = id;
+        this.title = title;
+        this.time = time;
+        this.value = value;
+        this.artist = artist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+
 }
