337cce5d8c46f05ed9edd41c204919930ef22931
corico44 <cairigaray999@gmail.com>
2018-11-25 04:31:28 +0100

    IS ALIVE!

--- app/src/main/java/lauzhack2018/music/music_runner/Player.java
+++ app/src/main/java/lauzhack2018/music/music_runner/Player.java
@@ -15,7 +15,8 @@ public class Player extends AppCompatActivity {
     private PlayerAdapter playerAdapter;
     PlaylistSingleton data;
     ArrayList<Song> playlist;
-
+    Button play_and_pause;
+    Button next_button;
     TextView songTitle;
     TextView songArtist;
 
@@ -32,23 +33,21 @@ public class Player extends AppCompatActivity {
         MediaPlayerManager manager = new MediaPlayerManager(this);
         playerAdapter = manager;
 
-        final Button playpauseButton = (Button) findViewById(R.id.play_pause_button);
-        playpauseButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (playerAdapter.isPlaying()) {
-                            playerAdapter.pause();
-                            //playpauseButton.setText("Play"); //En vez de texto, hay que poner imagen
-                        }
-                        else {
-                            playerAdapter.play();
-                            //playpauseButton.setText("Pause");
-                        }
-                    }
+        play_and_pause = findViewById(R.id.play_pause_button);
+        play_and_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playerAdapter.isPlaying()) {
+                    play_and_pause.setBackgroundResource(R.drawable.pause);
+                } else {
+                    play_and_pause.setBackgroundResource(R.drawable.play);
                 }
-        );
+            }
+
+        });
     }
+
+
 
     @Override
     protected void onStart() {
@@ -58,4 +57,16 @@ public class Player extends AppCompatActivity {
         songArtist.setText(playlist.get(0).artist);
     }
 
+    public void continue_song(){
+             int i = 1;
+            while(i<playlist.size())
+
+    {
+        playerAdapter.loadSong(playlist.get(i).id);
+        songTitle.setText(playlist.get(i).title);
+        songArtist.setText(playlist.get(i).artist);
+        ++i;
+    }
 }
+    }
+
